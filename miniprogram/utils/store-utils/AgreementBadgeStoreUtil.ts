ede753311e15c19dfd1313eba1126d33f51ec290
composable-tu <composable@foxmail.com>
2025-09-10 22:14:55 +0800

    refactor(miniprogram): 重构用户协议和隐私政策版本检查逻辑
    - 将协议和政策信息抽象为 DOCS 常量，简化代码结构
    - 使用循环替代重复代码，提高可维护性
    - 优化版本检查和徽标显示逻辑
    - 统一处理本地存储和徽标隐藏操作

--- miniprogram/utils/store-utils/AgreementBadgeStoreUtil.ts
+++ miniprogram/utils/store-utils/AgreementBadgeStoreUtil.ts
@@ -9,14 +9,35 @@ import {getWeixinStorageWithDefault} from "../WeixinStorageUtil";
 
 const semver = require('semver')
 
+const DOCS = [{
+    key: 'UserAgreement',
+    version: () => USER_AGREEMENT_VERSION,
+    storageKey: 'nowUserAgreementVersion',
+    badgeSetter: 'setIsShowUserAgreementBadge' as const
+}, {
+    key: 'PrivacyPolicy',
+    version: () => PRIVACY_POLICY_VERSION,
+    storageKey: 'nowPrivacyPolicyVersion',
+    badgeSetter: 'setIsShowPrivacyPolicyBadge' as const
+}, {
+    key: 'PersonalInformationCollectionList',
+    version: () => PERSONAL_INFORMATION_COLLECTION_LIST_VERSION,
+    storageKey: 'nowPersonalInformationCollectionListVersion',
+    badgeSetter: 'setIsShowPersonalInformationCollectionListBadge' as const
+}, {
+    key: 'ThirdPartyPersonalInformationSharingList',
+    version: () => THIRD_PARTY_PERSONAL_INFORMATION_SHARING_LIST_VERSION,
+    storageKey: 'nowThirdPartyPersonalInformationSharingListVersion',
+    badgeSetter: 'setIsShowThirdPartyPersonalInformationSharingListBadge' as const
+}] as const
+
 export const agreementBadgeStoreUtil = {
     checkAgreementBadgeStatus: async function (that: WechatMiniprogram.Page.TrivialInstance | WechatMiniprogram.Component.TrivialInstance) {
         const jwt = that.getJWT();
         if (!isLogin(jwt)) {
-            that.setIsShowUserAgreementBadge(false);
-            that.setIsShowPrivacyPolicyBadge(false);
-            that.setIsShowPersonalInformationCollectionListBadge(false);
-            that.setIsShowThirdPartyPersonalInformationSharingListBadge(false);
+            DOCS.forEach(d => {
+                that[d.badgeSetter](false);
+            });
         } else await getAgreementBadgeStatus(that);
     }, storeBinding: {
         fields: ['isShowUserAgreementBadge', 'isShowPrivacyPolicyBadge', 'isShowPersonalInformationCollectionListBadge', 'isShowThirdPartyPersonalInformationSharingListBadge'],
@@ -25,39 +46,31 @@ export const agreementBadgeStoreUtil = {
 }
 
 async function getAgreementBadgeStatus(that: WechatMiniprogram.Page.TrivialInstance | WechatMiniprogram.Component.TrivialInstance) {
-    const nowUserAgreementVersion = USER_AGREEMENT_VERSION
-    const nowPrivacyPolicyVersion = PRIVACY_POLICY_VERSION
-    const nowPersonalInformationCollectionListVersion = PERSONAL_INFORMATION_COLLECTION_LIST_VERSION
-    const nowThirdPartyPersonalInformationSharingListVersion = THIRD_PARTY_PERSONAL_INFORMATION_SHARING_LIST_VERSION
-    const [storageUserAgreementVersion, storagePrivacyPolicyVersion, storagePersonalInformationCollectionListVersion, storageThirdPartyPersonalInformationSharingListVersion] = await Promise.all([getWeixinStorageWithDefault<string>('nowUserAgreementVersion', "EMPTY", false), getWeixinStorageWithDefault<string>('nowPrivacyPolicyVersion', "EMPTY", false), getWeixinStorageWithDefault<string>('nowPersonalInformationCollectionListVersion', "EMPTY", false), getWeixinStorageWithDefault<string>('nowThirdPartyPersonalInformationSharingListVersion', "EMPTY", false)]);
-    that.setIsShowUserAgreementBadge(storageUserAgreementVersion === "EMPTY" || semver.gt(nowUserAgreementVersion, storageUserAgreementVersion));
-    that.setIsShowPrivacyPolicyBadge(storagePrivacyPolicyVersion === "EMPTY" || semver.gt(nowPrivacyPolicyVersion, storagePrivacyPolicyVersion))
-    that.setIsShowPersonalInformationCollectionListBadge(storagePersonalInformationCollectionListVersion === "EMPTY" || semver.gt(nowPersonalInformationCollectionListVersion, storagePersonalInformationCollectionListVersion));
-    that.setIsShowThirdPartyPersonalInformationSharingListBadge(storageThirdPartyPersonalInformationSharingListVersion === "EMPTY" || semver.gt(nowThirdPartyPersonalInformationSharingListVersion, storageThirdPartyPersonalInformationSharingListVersion));
+    const stored = await Promise.all(DOCS.map(d => getWeixinStorageWithDefault<string>(d.storageKey, EMPTY, false)))
+
+    DOCS.forEach((d, idx) => {
+        const nowV = d.version()
+        const storedV = stored[idx]
+        const show = storedV === EMPTY || semver.gt(nowV, storedV)
+        that[d.badgeSetter](show)
+    })
 }
 
 /**
  * 存储当前法律文本版本
  */
 export function setNowAgreementDocsVersionsStorage(that: WechatMiniprogram.Page.TrivialInstance | WechatMiniprogram.Component.TrivialInstance) {
-    wx.setStorageSync('nowUserAgreementVersion', USER_AGREEMENT_VERSION);
-    wx.setStorageSync('nowPrivacyPolicyVersion', PRIVACY_POLICY_VERSION);
-    wx.setStorageSync('nowPersonalInformationCollectionListVersion', PERSONAL_INFORMATION_COLLECTION_LIST_VERSION);
-    wx.setStorageSync('nowThirdPartyPersonalInformationSharingListVersion', THIRD_PARTY_PERSONAL_INFORMATION_SHARING_LIST_VERSION);
-    that.setIsShowUserAgreementBadge(false);
-    that.setIsShowPrivacyPolicyBadge(false);
-    that.setIsShowPersonalInformationCollectionListBadge(false);
-    that.setIsShowThirdPartyPersonalInformationSharingListBadge(false);
+    DOCS.forEach(d => {
+        wx.setStorageSync(d.storageKey, d.version())
+        that[d.badgeSetter](false)
+    })
 }
 
 export function cleanAgreementDocsVersionsStorage(that: WechatMiniprogram.Page.TrivialInstance | WechatMiniprogram.Component.TrivialInstance) {
-    const EMPTY = "EMPTY";
-    wx.setStorageSync('nowUserAgreementVersion', EMPTY);
-    wx.setStorageSync('nowPrivacyPolicyVersion', EMPTY);
-    wx.setStorageSync('nowPersonalInformationCollectionListVersion', EMPTY);
-    wx.setStorageSync('nowThirdPartyPersonalInformationSharingListVersion', EMPTY);
-    that.setIsShowUserAgreementBadge(false);
-    that.setIsShowPrivacyPolicyBadge(false);
-    that.setIsShowPersonalInformationCollectionListBadge(false);
-    that.setIsShowThirdPartyPersonalInformationSharingListBadge(false);
+    DOCS.forEach(d => {
+        wx.setStorageSync(d.storageKey, EMPTY)
+        that[d.badgeSetter](false)
+    })
 }
+
+const EMPTY = 'EMPTY'
